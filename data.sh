7478143b7dd0089ecb7071b3b4405e7d4ef2c861
Javier Dario VEGA BAEZ <66302636+dario-vega@users.noreply.github.com>
2021-09-14 09:38:03 +0200

    Update data.sh

--- data.sh
+++ data.sh
@@ -1,5 +1,14 @@
 mkdir ~/BaggageData
 cd ~/BaggageData
-curl https://raw.githubusercontent.com/dario-vega/ndcs_baggage_tracking_demo/main/data/generated_data/BaggageData.tar.gz -o BaggageData.tar.gz
+curl https://objectstorage.us-ashburn-1.oraclecloud.com/p/LxPTqOqEx3GoRb7myZ3VfDoFsj6lTACv-NpLLT7P9D8gHNgXVXCx03WHhx1MJT4Y/n/c4u04/b/data-management-library-files/o/nosql/BaggageData.tar.gz -o BaggageData.tar.gz
 tar xvzf BaggageData.tar.gz
 rm  BaggageData.tar.gz
+
+# Create a file to do a multi line load
+rm -f load_multi_line.json
+for file in `ls -1 ~/BaggageData/baggage_data* | tail -20`; do
+  echo $file
+  cat $file | tr '\n' ' ' >> load_multi_line.json
+  echo >> load_multi_line.json
+done
+
