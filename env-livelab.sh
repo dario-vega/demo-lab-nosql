c7547228a24009ca147830dc4de7a7955ae5cde9
Javier Dario VEGA BAEZ <66302636+dario-vega@users.noreply.github.com>
2021-09-24 14:37:32 +0200

    Update env-livelab.sh

--- env-livelab.sh
+++ env-livelab.sh
@@ -1,16 +1,14 @@
 # Please modify the following to variables
 export CMP_ID=your_compartment_ocid
 export NOSQL_USER_ID=your_user_ocid
+export NOSQL_FINGERPRINT=your_fingerprint
 
 export COMP_ID=${CMP_ID}
 export NOSQL_COMP_ID=${CMP_ID}
-export NOSQL_FINGERPRINT=ab:8c:f1:98:a8:53:d7:1f:ac:2e:92:4a:47:80:1b:f4
 export NOSQL_PRIVKEY_FILE=~/NoSQLLabPrivateKey.pem
 
 
-export NOSQL_ALWAYS_FREE=true
-
-if [ $CMP_ID == "your_compartment_ocid" ] || [ $NOSQL_USER_ID == "your_user_ocid" ]
+if [ $CMP_ID == "your_compartment_ocid" ] || [ $NOSQL_USER_ID == "your_user_ocid" ] || [ $NOSQL_FINGERPRINT == "your_fingerprint"  ]
 then
   echo "ERROR: Please review the configuration - missing API key parameters"
   return
@@ -21,6 +19,14 @@ then
   echo "ERROR: Please review the configuration - missing NOSQL_PRIVKEY_FILE file"
   return
 fi
+
+if [ $OCI_REGION == 'us-phoenix-1' ]
+then
+  export NOSQL_ALWAYS_FREE=true
+else
+  export NOSQL_ALWAYS_FREE=false
+fi
+
 
 echo $OCI_REGION
 echo $OCI_TENANCY
