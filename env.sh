1f8de2991ae941e9752fc520ef6ea0a920158f1e
Javier Dario VEGA BAEZ <66302636+dario-vega@users.noreply.github.com>
2021-09-24 06:30:43 +0200

    Update env.sh

--- env.sh
+++ env.sh
@@ -1,7 +1,6 @@
 CMP_ID=`oci iam compartment list --name  demonosql  --compartment-id $OCI_TENANCY | jq -r '."data"[].id'`
-# Advanced user, if you deploy in a compartment other than root or root/demonosql, change the following line with the good compartment_ocid and unconmmented
+# Advanced user, if you deploy in a compartment other than root or root/demonosql, change the following line with the good compartment_ocid and unconmment
 # CMP_ID=<your_compartment ocid>
-export COMP_ID=${CMP_ID-$OCI_TENANCY}
 
 export COMP_ID=${CMP_ID-$OCI_TENANCY}
 export NOSQL_COMP_ID=${CMP_ID-$OCI_TENANCY}
