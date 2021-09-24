86a9fe51fea40a7b06daf2d948b661058eabaad0
Javier Dario VEGA BAEZ <66302636+dario-vega@users.noreply.github.com>
2021-09-24 14:40:17 +0200

    Update env-freetier.sh

--- env-freetier.sh
+++ env-freetier.sh
@@ -7,12 +7,6 @@ export NOSQL_COMP_ID=${CMP_ID-$OCI_TENANCY}
 export NOSQL_USER_ID=`cat ~/info.json | jq -r '."data"."user-id"'`
 export NOSQL_FINGERPRINT=`cat ~/info.json | jq -r '."data"."fingerprint"'`
 export NOSQL_PRIVKEY_FILE=~/NoSQLLabPrivateKey.pem
-echo $OCI_REGION
-echo $OCI_TENANCY
-echo $NOSQL_USER_ID
-echo $NOSQL_FINGERPRINT
-echo $NOSQL_PRIVKEY_FILE
-echo $NOSQL_COMP_ID
 
 if [ $OCI_REGION == 'us-phoenix-1' ]
 then
@@ -20,3 +14,11 @@ then
 else
   export NOSQL_ALWAYS_FREE=false
 fi
+
+echo $OCI_REGION
+echo $OCI_TENANCY
+echo $NOSQL_USER_ID
+echo $NOSQL_FINGERPRINT
+echo $NOSQL_PRIVKEY_FILE
+echo $NOSQL_COMP_ID
+
