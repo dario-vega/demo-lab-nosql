c778377a1727f7c48aab0856333fbf55de3708bd
Javier Dario VEGA BAEZ <66302636+dario-vega@users.noreply.github.com>
2022-03-30 13:31:11 +0200

    Update data.sh

--- data.sh
+++ data.sh
@@ -1,6 +1,6 @@
 mkdir ~/BaggageData
 cd ~/BaggageData
-curl https://objectstorage.us-ashburn-1.oraclecloud.com/p/WwljGusAgTOa0HNqK7f_8dV0jGo_DzNg7ZnMIlvpDMDbD5PmTcBFDfUut6JvTbiH/n/c4u04/b/data-management-library-files/o/BaggageData.tar.gz -o BaggageData.tar.gz
+curl https://objectstorage.us-ashburn-1.oraclecloud.com/p/GzwEz9xBCBHrd0gx1QE0U8hqvNTKzmKcs1pSx3CQ2Ip9A05Z1vHgPNeVRMx_1cLp/n/c4u04/b/livelabsfiles/o/data-management-library-files/BaggageData.tar.gz -o BaggageData.tar.gz
 tar xvzf BaggageData.tar.gz
 rm  BaggageData.tar.gz
 
